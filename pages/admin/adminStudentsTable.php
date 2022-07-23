3d455e90ccbb8e1e8b2b2f0b5b34b488af4bcda8
Richae <mnrichae@gmail.com>
2022-07-23 18:07:06 +0800

    s&t view for admin

--- pages/admin/adminStudentsTable.php
+++ pages/admin/adminStudentsTable.php
@@ -5,6 +5,17 @@
         <i class="fa-solid fa-users"></i>
         <span class="text">Students</span>
     </div>
+     <!--alert message-->
+     <div>
+        <?php
+            if (isset($_GET['errorDelStudent'])){?>
+                <p class = "alert alert-danger mt-2" ><i class='bi bi-exclamation-circle-fill'></i> <?php echo $_GET['errorDelStudent']; ?></p>    
+            <?php }?>
+            <?php
+                if (isset($_GET['successDelStudent'])){?>
+                    <p class = "alert alert-success mt-2" ><i class="bi bi-check-circle-fill"></i> <?php echo $_GET['successDelStudent']; ?></p>    
+            <?php }?>
+    </div>
     <div class="box">
     <table class="table table-striped">
         <thead>
@@ -24,10 +35,10 @@
                     <td><?php echo $student['email'] ?></td>
                     <td><?php echo $student['mobile_number'] ?></td>
                     <td>
-                        <form action="./inc/delete.php" method="post">
+                        <form action="./inc/actionStudent.php" method="post">
                                 <input type="hidden" name="id" id="id" value="<?php echo $student['user_id'] ?>">
                                 <input type="hidden" name="table" id="table" value="students">
-                                <input type="submit" class="btn btn-success" value="View" name="update">
+                                <input type="submit" class="btn btn-success" value="View" name="view">
                                 <input type="submit" class="btn btn-danger" value="Delete" name="delete" onclick="return confirm('Are you sure you want to delete this record?')">
                         </form>
                     </td>
