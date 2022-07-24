a9a2dcb773626ae24bf4dc9fcb06840b0295f5b3
Richae <mnrichae@gmail.com>
2022-07-24 17:51:45 +0800

    added color coding to status

--- pages/tutors/tutorsStudentTable.php
+++ pages/tutors/tutorsStudentTable.php
@@ -22,19 +22,29 @@
         <tbody>
             <?php 
             while ($student = mysqli_fetch_array($sql_get_tutorsStudent)){
-            $btn= false;
+                $btn = false;
+                $appointment_date = date($appointment['date']);
+                $date_appointed = new DateTime($appointment_date);
+                $date_now = new DateTime();
+                if($date_appointed < $date_now){
+                    $appointment['status'] = 6;
+                }
                 if($student['status']==2){
                     $status = "Pending";
+                    $color = 'text-warning';
                     $btn = true;
-                }
-                elseif($student['status']==3){
+                } elseif($student['status']==3){
                     $status = "Approved";
-                }
-                elseif($student['status']==4){
-                    $status = "Canceled";
-                }
-                elseif($student['status']==5){
+                    $color = 'text-info';
+                } elseif($student['status']==4){
+                    $status = "Cancelled";
+                    $color = 'text-danger';
+                } elseif($student['status']==5){
                     $status = "Completed";
+                    $color = 'text-success';
+                } elseif($appointment['status'] == 6){
+                    $status = "Expired";
+                    $color = 'text-muted';
                 }
                 
                 ?>
@@ -44,13 +54,13 @@
                     <td><?php echo $student['subject'] ?></td>
                     <td><?php echo $student['date'] ?></td>
                     <td><?php echo $student['time'] ?></td>
-                    <td><?php echo $status ?></td>
+                    <td class="<?php echo $color?>"><?php echo $status ?></td>
                     <td>
                         <form action="./inc/update.php" method="post">
                                 <input type="hidden" name="id" id="id" value="<?php echo $student['appointment_id'] ?>">
                                 <input type="hidden" name="table" id="table" value="students">
-                                <input type="submit" class="btn btn-success" value="Cancel" name="cancel" <?php if(!$btn)echo "disabled"?>>
-                                <input type="submit" class="btn btn-danger" value="Approve" name="approve" <?php if(!$btn)echo "disabled"?> onclick="return confirm('Are you sure you want to delete this record?')">
+                                <input type="submit" class="btn btn-success" value="Cancel" name="cancel" <?php if(!$btn)echo "disabled"?> onclick="return confirm('Are you sure you want to delete this record?')">
+                                <input type="submit" class="btn btn-danger" value="Approve" name="approve" <?php if(!$btn)echo "disabled"?> >
                         </form>
                     </td>
                 </tr>
